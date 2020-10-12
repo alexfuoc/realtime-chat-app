b331647f4204f48d83eebb6acea823465b6c71f8
alexfuoc <53452014+alexfuoc@users.noreply.github.com>
2020-10-12 19:43:17 -0400

    Express folder struct added
    Changed client files, express app created

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React I'm DOING ITTTTTT
+          Link to docs
         </a>
       </header>
     </div>
